003a6d011b484c4be1bca2e8e6be9a4967f30ca5
democxt <democxt@certincloud.com>
2016-09-27 02:00:20 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php 
- echo "I am VERSION 1 <br><br>";
- echo "My Pod IP is : ".$_SERVER['SERVER_ADDR'] ;
+ echo "Now I am VERSION 2 <br><br>";
+ echo "And My Pod IP is : ".$_SERVER['SERVER_ADDR'] ;
  ?>
