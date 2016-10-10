a1801b5b79f97dfca1ca16c78c1751ca2ac7d14a
democxt <democxt@certincloud.com>
2016-10-10 13:43:59 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
- echo "I am VERSION 2 Now!! <br><br>";
+ echo "I am VERSION 3 and PHP7.0.11 Now!! <br><br>";
  echo "My Pod ID is :";
  echo gethostname();
  echo "<br><br>My IP is : ".$_SERVER['SERVER_ADDR'] ;
